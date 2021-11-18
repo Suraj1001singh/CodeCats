8c6c2cb9482a1227cfa473ff16590ecc3094092c
Suraj Singh <absurajsinghchamiyal@gmail,com>
2021-11-18 18:47:14 +0530

    task2: Swap Numbers Using Call by Reference

---
+++ Week5/task2/test.c
@@ -0,0 +1,18 @@
+// Program Swap Numbers Using Call by Reference
+#include <stdio.h>
+void swap(int *a, int *b)
+{
+
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    swap(&a, &b);
+    printf("%d %d", a, b);
+    return 0;
+}
